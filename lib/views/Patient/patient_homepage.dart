9aebe74c2d696e645d61548a3ee494f5fe8e0361
ianwambaire <ian.wambaire@strathmore.edu>
2025-06-15 15:34:39 +0300

    added the PatientHomepageState class

--- lib/views/Patient/patient_homepage.dart
+++ lib/views/Patient/patient_homepage.dart
@@ -3,5 +3,14 @@ import 'package:flutter/material.dart';
 class PatientHomepage extends StatefulWidget {
   const PatientHomepage({super.key});
   @override
-  State<PatientHomepage> createState() => _patientHomepageState();
+  State<PatientHomepage> createState() => _PatientHomepageState();
+}
+
+class _PatientHomepageState extends State<PatientHomepage> {
+  int _selectedIndex = 0;
+  void _whenNavisTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
