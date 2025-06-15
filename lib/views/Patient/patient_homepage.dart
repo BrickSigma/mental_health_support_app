6ef73fae266a4da5aaa65ea5fafb49664ae75db3
ianwambaire <ian.wambaire@strathmore.edu>
2025-06-15 15:28:12 +0300

    created the patient_homepage file

---
+++ lib/views/Patient/patient_homepage.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class PatientHomepage extends StatefulWidget {
+  const PatientHomepage({super.key});
+  @override
+  State<PatientHomepage> createState() => _patientHomepageState();
+}
